b7ac1707f6c727674a46186367683fadf4e64dab
Firaol Mk <firaolmekbib511@gmail.com>
2024-06-14 22:11:08 +0300

    updated

---
+++ API/run.py
@@ -0,0 +1,13 @@
+# run/wsgi used to start flask application
+
+from app import create_app, db
+from app.models import User
+
+app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
+
+if __name__ == '__main__':
+    app.run(debug=True)
